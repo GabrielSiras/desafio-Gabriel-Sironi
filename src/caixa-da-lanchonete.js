dcc287e3142613e48b6ad5a32092bd4df2950902
Gabriel <gabrelsironi06@gmail.com>
2023-08-16 09:47:03 -0300

    Continuando o setup inicial

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,9 +1,31 @@
 class CaixaDaLanchonete {
-
-    calcularValorDaCompra(metodoDePagamento, itens) {
-        return "";
+    new CaixaDaLanchonete(){
+        
+    var cardapio = {
+  cafe: 3.00,
+  chantily: 1.50,
+  suco: 6.20,
+  sanduiche: 6.50,
+  queijo: 2.00,
+  salgado: 7.25,
+  combo1: 9.50,
+  combo2: 7.50
+};
+    function calcularValorDaCompra(metodoDePagamento, itens) {
+        let total = 0;
+        for(const item of itens){
+            if(cardapio[item])
+            calcularValorDaCompra += cardapio[item];
+        } else {
+        console.log(`Item '${item}' Item inválido!`)
     }
-
 }
+       
+        return total;
+}
+const 
+    
+
 
 export { CaixaDaLanchonete };
+}
