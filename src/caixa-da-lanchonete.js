1c3a61c06dfd4278db50c5ab2e7703b1ab64a5d1
Gabriel <gabrelsironi06@gmail.com>
2023-08-16 16:16:12 -0300

    Atualização da caixa registradora.

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,57 +1,49 @@
+const cardapio = {
+    "cafe": 3.00,
+};
 class CaixaDaLanchonete {
-    calcularValorDaCompra(metodoDePagamento, itens, number) {
+    constructor() { }
 
-        const texto = 'Bem vindo(a) a lanchonete da DB!\nA seguir segue o nosso cardápio:\n'
-        console.log(cafe);
-
-        let cafe = 3.00;
-
+    calcularValorDaCompra(metodoDePagamento, itens) {
         const descontoDinheiro = 0.05;
 
         let valorTotal = 0;
+
+        if (itens.length === 0) {
+            return "Não há itens no carrinho de compra!";
+        }
 
         for (const item of itens) {
             const nomeItem = item.nome;
             const quantidade = item.quantidade || 1;
             const valorItem = cardapio[nomeItem];
 
-            if (valorItem != cardapio[nomeItem]) {
+            if (valorItem === undefined) {
                 return "Item inválido!"
             }
 
             if (nomeItem.includes("extra")) {
                 const itemPrincipal = nomeItem.split(" (extra")[0];
                 if (!itens.find(it => it.nome === itemPrincipal)) {
-                    return "Item extra não pode ser pedido sem o principal";
+                    return "Item extra não pode ser pedido sem o principal!";
                 }
             }
             valorTotal += valorItem * quantidade;
-
-            if (itens.length === 0) {
-                return "Não há itens no carrinho de compra!";
-            }
 
             if (valorTotal === 0) {
                 return "Quantidade inválida!";
             }
 
-            switch (metodoDePagamento) {
-                case "Dinheiro":
-                    valorTotal -= valorTotal * descontoDinheiro;
-                    return `R$ ${valorFinal}`;
-                    break;
-                    valorTotal -= valorTotal * descontoDinheiro;
+            if (metodoDePagamento === "Dinheiro") {
+                valorTotal -= valorTotal * descontoDinheiro;
             }
 
             const valorFinal = valorTotal.toFixed(2);
 
             return `R$ ${valorFinal}`;
+
         }
+        return "CaixaDaLanchonete";
     }
-
 }
-    const mensagemCompra = calcularValorDaCompra(itens, metodoDePagamento);
-    console.log(mensagemCompra);
-    return "";
-
 export { CaixaDaLanchonete };
