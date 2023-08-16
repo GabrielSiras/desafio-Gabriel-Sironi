a4efb90c578e513ec1dd0edaa474bc93b5a6fc3b
Gabriel <gabrelsironi06@gmail.com>
2023-08-16 13:45:36 -0300

    Atualização da caixa registradora.

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,69 +1,57 @@
 class CaixaDaLanchonete {
-    function calcularValorDaCompra(metodoDePagamento, itens){
+    calcularValorDaCompra(metodoDePagamento, itens, number) {
 
-        const cardapio = {
-  cafe: 3.00,
-  chantily: 1.50,
-  suco: 6.20,
-  sanduiche: 6.50,
-  queijo: 2.00,
-  salgado: 7.25,
-  combo1: 9.50,
-  combo2: 7.50
-};
+        const texto = 'Bem vindo(a) a lanchonete da DB!\nA seguir segue o nosso cardápio:\n'
+        console.log(cafe);
+
+        let cafe = 3.00;
 
         const descontoDinheiro = 0.05;
 
-        const acrescimoCredito = 0.03;
-
         let valorTotal = 0;
 
-        for(const item of itens){
+        for (const item of itens) {
             const nomeItem = item.nome;
             const quantidade = item.quantidade || 1;
             const valorItem = cardapio[nomeItem];
 
-            if(!valorItem) {
+            if (valorItem != cardapio[nomeItem]) {
                 return "Item inválido!"
-                }
-                
-            if (nomeItem.includes("extra")){
+            }
+
+            if (nomeItem.includes("extra")) {
                 const itemPrincipal = nomeItem.split(" (extra")[0];
-            if (!itens.find(it => it.nome === itemPrincipal)){
+                if (!itens.find(it => it.nome === itemPrincipal)) {
                     return "Item extra não pode ser pedido sem o principal";
                 }
             }
-                valorTotal += valorItem * quantidade;
+            valorTotal += valorItem * quantidade;
 
-            if (itens.length === 0){
+            if (itens.length === 0) {
                 return "Não há itens no carrinho de compra!";
             }
 
-            if (valorTotal === 0){
+            if (valorTotal === 0) {
                 return "Quantidade inválida!";
             }
 
-            if (!["Dinheiro", "Débito", "Crédito"].includes(metodoDePagamento)){
-                return "Forma de pagamento inválida!";
-            }
-
-            if (metodoDePagamento === "Dinheiro"){
-                valorTotal -= valorTotal * descontoDinheiro;
-            } else if (metodoDePagamento === "Crédito"){
-                valorTotal += valorTotal * acrescimoCredito;
+            switch (metodoDePagamento) {
+                case "Dinheiro":
+                    valorTotal -= valorTotal * descontoDinheiro;
+                    return `R$ ${valorFinal}`;
+                    break;
+                    valorTotal -= valorTotal * descontoDinheiro;
             }
 
             const valorFinal = valorTotal.toFixed(2);
 
-            if (metodoDePagamento === "Dinheiro"){
-                return `R$ ${valorFinal}`;
-            } else if (metodoDePagamento === "Crédito"){
-                return `R$ ${valorFinal}`;
-            } else {
-                return `R$ ${valorFinal}`
-            }
+            return `R$ ${valorFinal}`;
+        }
     }
 
 }
+    const mensagemCompra = calcularValorDaCompra(itens, metodoDePagamento);
+    console.log(mensagemCompra);
+    return "";
 
 export { CaixaDaLanchonete };
